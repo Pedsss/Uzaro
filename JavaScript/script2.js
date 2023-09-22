03ac49614b408fb4d4ba60ed4d1946357e0d97f4
Pedsss <vincentabuel00@gmail.com>
2023-09-23 03:26:01 +0800

    updated, remove some things

--- JavaScript/script2.js
+++ JavaScript/script2.js
@@ -1,17 +1,8 @@
-const btn = document.getElementById('contactBtn');
-const targetDiv = document.getElementById('sectionContainer3');
 const nav = document.querySelector('.nav');
 const hamburger = document.querySelector('.hamburger');
-
-
-btn.addEventListener("click", scrollToBottom);
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("active");
     nav.style.width = "100%";
     hamburger.classList.toggle("change");
 });
-
-function scrollToBottom(){
-    targetDiv.scrollIntoView({behavior: "smooth"});
-}
